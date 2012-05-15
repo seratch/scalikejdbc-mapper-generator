2c7df71cccbb614e75f26ca8ab76d3d74b3ab19a
Kazuhiro Sera <seratch@gmail.com>
2012-05-16 01:37:55 +0900

    Fixed issue #4 Improve the way to configure

--- src/main/scala/scalikejdbc/mapper/SbtPlugin.scala
+++ src/main/scala/scalikejdbc/mapper/SbtPlugin.scala
@@ -17,49 +17,72 @@ package scalikejdbc.mapper
 
 import sbt._
 import sbt.Keys._
+import util.control.Exception._
 
 object SbtPlugin extends Plugin {
 
   import SbtKeys._
 
+  case class JDBCSettings(driver: String, url: String, username: String, password: String, schema: String)
+  case class GeneratorSetings(packageName: String, template: String, lineBreak: String, encoding: String)
+
+  def loadSettings(): (JDBCSettings, GeneratorSetings) = {
+    val props = new java.util.Properties
+    using(new java.io.FileInputStream("project/scalikejdbc-mapper-generator.properties")) {
+      inputStream => props.load(inputStream)
+    }
+    (JDBCSettings(
+      driver = props.get("jdbc.driver").toString,
+      url = props.get("jdbc.url").toString,
+      username = props.get("jdbc.username").toString,
+      password = props.get("jdbc.password").toString,
+      schema = props.get("jdbc.schema").toString
+    ), GeneratorSetings(
+        packageName = props.get("generator.packageName").toString,
+        template = props.get("generator.template").toString,
+        lineBreak = props.get("generator.lineBreak").toString,
+        encoding = props.get("generator.encoding").toString
+      ))
+  }
+
   val genTask = inputTask {
     (task: TaskKey[Seq[String]]) =>
-      (task, scalaSource in Compile,
-        scalikejdbcDriver, scalikejdbcUrl, scalikejdbcUsername, scalikejdbcPassword, scalikejdbcSchema,
-        scalikejdbcPackageName, scalikejdbcLineBreak) map {
-          case (args, srcDir, driver, url, username, password, schema, packageName, lineBreak) =>
-            args match {
-              case Nil => println("Usage: scalikejdbc-gen [table-name]")
-              case _ =>
-                Class.forName(driver) // load specified jdbc driver
-                val tableName = args.head
-                val model = Model(url, username, password)
-                model.table(schema, tableName)
-                  .orElse(model.table(schema, tableName.toUpperCase))
-                  .orElse(model.table(schema, tableName.toLowerCase))
-                  .map { table =>
+      (task, scalaSource in Compile, scalaSource in Test) map {
+        case (args, srcDir, testDir) =>
+          args match {
+            case Nil => println("Usage: scalikejdbc-gen [table-name]")
+            case _ =>
+              val (jdbc, generatorSettings) = loadSettings()
+              Class.forName(jdbc.driver) // load specified jdbc driver
+              val tableName = args.head
+              val model = Model(jdbc.url, jdbc.username, jdbc.password)
+              model.table(jdbc.schema, tableName)
+                .orElse(model.table(jdbc.schema, tableName.toUpperCase))
+                .orElse(model.table(jdbc.schema, tableName.toLowerCase))
+                .map {
+                  table =>
                     ARLikeTemplateGenerator(table)(GeneratorConfig(
                       srcDir = srcDir.getAbsolutePath,
-                      packageName = packageName,
-                      lineBreak = lineBreak
+                      testDir = testDir.getAbsolutePath,
+                      packageName = generatorSettings.packageName,
+                      template = GeneratorTemplate(generatorSettings.template),
+                      lineBreak = LineBreak(generatorSettings.lineBreak),
+                      encoding = generatorSettings.encoding
                     )).writeFileIfNotExist()
-                  } getOrElse {
-                    println("The table is not found.")
-                  }
-            }
-        }
+                } getOrElse {
+                  println("The table is not found.")
+                }
+          }
+      }
   }
 
   val scalikejdbcSettings = inConfig(Compile)(Seq(
-    scalikejdbcGen <<= genTask,
-    scalikejdbcDriver := "",
-    scalikejdbcUrl := "",
-    scalikejdbcUsername := "",
-    scalikejdbcPassword := "",
-    scalikejdbcSchema := "",
-    scalikejdbcPackageName := "",
-    scalikejdbcLineBreak := "\n"
+    scalikejdbcGen <<= genTask
   ))
+
+  def using[R <: { def close() }, A](resource: R)(f: R => A): A = ultimately {
+    ignoring(classOf[Throwable]) apply resource.close()
+  } apply f(resource)
 
 }
 
